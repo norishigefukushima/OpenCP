447583871b5375bf25d4e9c4b59ed0db196dc0de
Norishige Fukushima <norishigefukushima@users.noreply.github.com>
2021-12-17 12:59:32 +0900

    update for up/down sampling

--- include/downsample.hpp
+++ include/downsample.hpp
@@ -26,6 +26,10 @@ namespace cp
 	//CP_LANCZOS:	order(4)
 	//CP_GAUSS:		sigma_clip(3.0)
 	//CP_GAUSS_FAST:sigma_clip(3.0)
+	//r = (scale/2)*radius_ratio
+	//Gauss: sigma = r/parameter
 	CP_EXPORT void downsample(cv::InputArray src, cv::OutputArray dest, const int scale, const Downsample downsample_method, const double parameter = 0.0, double radius_ratio = 1.0);
+	// add last element for cp::downsample
+	CP_EXPORT void downsamplePlus1(cv::InputArray src, cv::OutputArray dest, const int scale, const Downsample downsample_method, const double parameter = 0.0, double radius_ratio = 1.0);
 	CP_EXPORT std::string getDowsamplingMethod(const Downsample method);
 }
