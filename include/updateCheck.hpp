97f0fa0fff6c68398acb3b4ac63ea3c5aba53374
Norishige Fukushima <norishigefukushima@users.noreply.github.com>
2023-06-12 17:15:24 +0900

    add print in updateCheck

--- include/updateCheck.hpp
+++ include/updateCheck.hpp
@@ -63,5 +63,7 @@ namespace cp
 		bool isUpdate(double p0, double p1, double p2, double p3, double p4, double p5, double p6, double p7, double p8, double p9, double p10, double p11, double p12, double p13);
 		bool isUpdate(double p0, double p1, double p2, double p3, double p4, double p5, double p6, double p7, double p8, double p9, double p10, double p11, double p12, double p13, double p14);
 		bool isUpdate(double p0, double p1, double p2, double p3, double p4, double p5, double p6, double p7, double p8, double p9, double p10, double p11, double p12, double p13, double p14, double p15);
+
+		void print(int num_args);
 	};
 }
