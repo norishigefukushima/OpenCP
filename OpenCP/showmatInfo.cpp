97827bb9b5b2e4803ffc20da685cf3cc1d82b063
labfukushima <lab.fukushima@gmail.com>
2023-08-14 12:59:10 +0900

    Update showmatInfo.cpp

--- OpenCP/showmatInfo.cpp
+++ OpenCP/showmatInfo.cpp
@@ -5,34 +5,38 @@ using namespace std;
 
 namespace cp
 {
-	static void showMatInfo_internal(cv::Mat src, string name, const bool isStatInfo)
+	static void showMatInfo_internal(cv::Mat& s, string name, const bool isStatInfo)
 	{
 		cout << "name    : " << name << endl;
-		if (src.empty())
+		if (s.empty())
 		{
 			cout << "empty" << endl;
 			return;
 		}
 
-		cout << "size[WxH]: " << src.size() << endl;
-		cout << "channel  : " << src.channels() << endl;
+		cout << "size[WxH]: " << s.size() << endl;
+		cout << "channel  : " << s.channels() << endl;
 		cout << "depth    : ";
-		if (src.depth() == CV_8U)cout << "8U" << endl;
-		else if (src.depth() == CV_8S)cout << "8S" << endl;
-		else if (src.depth() == CV_16S)cout << "16S" << endl;
-		else if (src.depth() == CV_16U)cout << "16U" << endl;
-		else if (src.depth() == CV_32S)cout << "32S" << endl;
-		else if (src.depth() == CV_32F)cout << "32F" << endl;
-		else if (src.depth() == CV_64F)cout << "64F" << endl;
-		else if (src.depth() == CV_16F)cout << "16F" << endl;
+		if (s.depth() == CV_8U)		  cout << "8U" << endl;
+		else if (s.depth() == CV_8S)  cout << "8S" << endl;
+		else if (s.depth() == CV_16S) cout << "16S" << endl;
+		else if (s.depth() == CV_16U) cout << "16U" << endl;
+		else if (s.depth() == CV_32S) cout << "32S" << endl;
+		else if (s.depth() == CV_32F) cout << "32F" << endl;
+		else if (s.depth() == CV_64F) cout << "64F" << endl;
+		else if (s.depth() == CV_16F) cout << "16F" << endl;
 		
-		if (src.isContinuous()) cout << "continue: true"<<endl;
-		else  cout << "continue: false" << endl;
-		if (src.isSubmatrix()) cout << "ROI     : true" << endl;
-		else  cout << "ROI     : false" << endl;
+		if (s.isContinuous())	cout << "continue: true" <<endl;
+		else					cout << "continue: false" << endl;
+		if (s.isSubmatrix())	cout << "ROI     : true" << endl;
+		else					cout << "ROI     : false" << endl;
 
-		if (src.channels() < 5)
+		if (s.channels() < 5)
 		{
+			Mat src;
+			if (s.depth() == CV_16F) s.convertTo(src, CV_32F);//cv::meanStdDev is not  for FP16 in OpenCV.
+			else src = s;
+			
 			Scalar v, stdv;
 			cv::meanStdDev(src, v, stdv);
 			
